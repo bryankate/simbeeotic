8bc8385412fc3feb5dece3f0a2676301d869e2d3
Bkate <Bkate@25a99819-ff93-4a19-9767-1b5daa2b054e>
2010-03-30 00:03:45 +0000

    added dBm utilities

--- simbeeotic-core/src/test/java/harvard/robobees/simbeeotic/util/MathUtilTest.java
+++ simbeeotic-core/src/test/java/harvard/robobees/simbeeotic/util/MathUtilTest.java
@@ -11,14 +11,14 @@ import javax.vecmath.Vector3f;
 /**
  * @author bkate
  */
-public class LinearMathUtilTest extends TestCase {
+public class MathUtilTest extends TestCase {
 
     public void testGetRotation() {
 
         Vector3f v1 = new Vector3f(0, 0, 1);
         Vector3f v2 = new Vector3f(0, 1, 0);
 
-        Quat4f rot = LinearMathUtil.getRotation(v1, v2);
+        Quat4f rot = MathUtil.getRotation(v1, v2);
 
         Vector3f unitZ = new Vector3f(0, 0, 1);
 
@@ -35,11 +35,24 @@ public class LinearMathUtilTest extends TestCase {
 
 
         // no rotation between parallel vectors
-        rot = LinearMathUtil.getRotation(v1, v1);
+        rot = MathUtil.getRotation(v1, v1);
 
         assertEquals("Parallel vectors have nonzero rotation!", rot.x, 0.0f);
         assertEquals("Parallel vectors have nonzero rotation!", rot.y, 0.0f);
         assertEquals("Parallel vectors have nonzero rotation!", rot.z, 0.0f);
         assertEquals("Parallel vectors have nonzero rotation!", rot.w, 0.0f);
+    }
+
+
+    public void testDbmConversion() {
+
+        assertEquals(MathUtil.mwToDbm(0), Double.NEGATIVE_INFINITY);
+        assertEquals(MathUtil.mwToDbm(1), 0.0, 1e-10);
+        assertEquals(MathUtil.mwToDbm(100), 20.0, 1e-10);
+        assertEquals(MathUtil.mwToDbm(0.1), -10.0, 1e-10);
+
+        assertEquals(MathUtil.dbmToMw(0), 1.0, 1e-10);
+        assertEquals(MathUtil.dbmToMw(-10), 0.1, 1e-10);
+        assertEquals(MathUtil.dbmToMw(20), 100.0, 1e-10);
     }
 }
