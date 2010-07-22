ba1671fe72164f4ab5439a8cf8c45701db00d455
bkate <bkate@25a99819-ff93-4a19-9767-1b5daa2b054e>
2010-07-22 02:52:00 +0000

    adding IO utility class

---
+++ simbeeotic-core/src/main/java/harvard/robobees/simbeeotic/util/IOUtil.java
@@ -0,0 +1,76 @@
+package harvard.robobees.simbeeotic.util;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+
+/**
+ * A class with utility methods for dealing with IO.
+ *
+ * @author bkate
+ */
+public class IOUtil {
+
+    /**
+     * Converts a list of objects to a byte stream. The objects can only
+     * be of the type {@code Number}, {@code Boolean}, {@code String}, or {@code byte[]}.
+     * Any other type of object will be skipped over.
+     *
+     * @see DataOutputStream for a description of the resulting byte order.
+     *
+     * @param objects The list of objects to encode.
+     *
+     * @return The list of objects, represented as a stream of bytes.
+     */
+    private static byte[] toBytes(Object ... objects) {
+
+        ByteArrayOutputStream arrayStream = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(arrayStream);
+
+        try {
+
+            for (Object obj : objects) {
+
+                if (obj instanceof Number) {
+
+                    Number num = (Number)obj;
+
+                    if (num instanceof Byte) {
+                        out.writeByte(num.byteValue());
+                    }
+                    else if (num instanceof Short) {
+                        out.writeShort(num.shortValue());
+                    }
+                    else if (num instanceof Integer) {
+                        out.writeInt(num.intValue());
+                    }
+                    else if (num instanceof Long) {
+                        out.writeLong(num.longValue());
+                    }
+                    else if (num instanceof Float) {
+                        out.writeFloat(num.floatValue());
+                    }
+                    else if (num instanceof Double) {
+                        out.writeDouble(num.doubleValue());
+                    }
+                }
+                else if (obj instanceof Boolean) {
+                    out.writeBoolean((Boolean)obj);
+                }
+                else if (obj instanceof String) {
+                    out.writeUTF((String)obj);
+                }
+                else if (obj instanceof byte[]) {
+                    out.write((byte[])obj);
+                }
+            }
+        }
+        catch(IOException ioe) {
+            throw new RuntimeException("Problem converting number to bytes.", ioe);
+        }
+
+        return arrayStream.toByteArray();
+    }
+}
