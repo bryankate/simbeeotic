ae3fb4bec3decc1adf34144b6b9be60bb7b993e7
bkate <bkate@25a99819-ff93-4a19-9767-1b5daa2b054e>
2011-03-02 23:47:47 +0000

    removed bad import

--- simbeeotic-testbed/src/main/java/harvard/robobees/simbeeotic/model/HWILBee.java
+++ simbeeotic-testbed/src/main/java/harvard/robobees/simbeeotic/model/HWILBee.java
@@ -10,14 +10,12 @@ import com.bulletphysics.linearmath.MotionState;
 import com.bulletphysics.linearmath.Transform;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
-import com.sun.xml.internal.ws.message.ByteArrayAttachment;
 import harvard.robobees.simbeeotic.SimTime;
 import harvard.robobees.simbeeotic.configuration.ConfigurationAnnotations.GlobalScope;
 import org.apache.log4j.Logger;
 
 import javax.vecmath.Vector3f;
 import java.awt.Color;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
