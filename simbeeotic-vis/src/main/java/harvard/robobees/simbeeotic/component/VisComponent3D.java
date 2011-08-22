4f8e8269cb54e7eacfc1fc398fdb1ca47d198022
bkate <bkate@25a99819-ff93-4a19-9767-1b5daa2b054e>
2011-08-22 19:59:29 +0000

    added params for demo

--- simbeeotic-vis/src/main/java/harvard/robobees/simbeeotic/component/VisComponent3D.java
+++ simbeeotic-vis/src/main/java/harvard/robobees/simbeeotic/component/VisComponent3D.java
@@ -4,8 +4,13 @@ package harvard.robobees.simbeeotic.component;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.WindowEvent;
 
 
@@ -24,30 +29,67 @@ public class VisComponent3D extends JFrame implements VariationComponent {
     @Inject(optional=true)
     @Named("use-background")
     private boolean useBackground = true;
-    
+
+
+    @Inject(optional=true)
+    @Named("show-controls")
+    private boolean showControls = true;
+
+
+    @Inject(optional=true)
+    @Named("testbed-view")
+    private boolean testbedView = false;
+
+
+    @Inject(optional=true)
+    @Named("second-screen")
+    private boolean secondScreen = false;
+
     
     @Override
     public void initialize() {
 
         Dimension size = new Dimension(900, 600);
+        JComponent contentPane;
 
         world = new Java3DWorld(useBackground);
 
-        ControlPanel control = new ControlPanel(world, context.getClockControl(), context.getSimEngine());
-        JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, world, control);
+        if (showControls) {
+
+            ControlPanel control = new ControlPanel(world, context.getClockControl(), context.getSimEngine());
+            JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, world, control);
+
+            pane.setPreferredSize(size);
+            pane.setResizeWeight(1.0);
+
+            pack();
+
+            pane.setDividerLocation(0.7);
+
+            contentPane = pane;
+        }
+        else {
+            contentPane = world;
+        }
+
+        if (testbedView) {
+            world.setMainView(new Point3d(0, 8, 0), new Point3d(0, 0, 0), new Vector3d(1, 0, 0));
+        }
 
         context.getRecorder().addListener(world);
 
-        pane.setPreferredSize(size);
-        pane.setResizeWeight(1.0);
-
         setTitle("3D Visualization");
-        setSize(size);
-        setContentPane(pane);
+        setContentPane(contentPane);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setVisible(true);
 
-        pane.setDividerLocation(0.7);
+        if (secondScreen) {
+
+            size = new Dimension(1024, 1040);
+            setLocation(1715, 20);
+        }
+
+        setSize(size);
+        setVisible(true);
     }
 
 
