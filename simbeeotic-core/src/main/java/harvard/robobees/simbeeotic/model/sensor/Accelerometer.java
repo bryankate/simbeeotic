9ce09fbe81dfd87abf432d03739aa909b818e305
bkate <bkate@25a99819-ff93-4a19-9767-1b5daa2b054e>
2011-12-05 02:05:12 +0000

    fixed documentation

--- simbeeotic-core/src/main/java/harvard/robobees/simbeeotic/model/sensor/Accelerometer.java
+++ simbeeotic-core/src/main/java/harvard/robobees/simbeeotic/model/sensor/Accelerometer.java
@@ -10,12 +10,24 @@ import javax.vecmath.Vector3f;
 public interface Accelerometer {
 
     /**
+     * <p>
      * Gets the linear acceleration of the body, as measured by
      * a 3-axis accelerometer. The readings are relative to the body frame,
      * meaning that the reading in the x direction is aligned down the
      * x axis of the body, regardless of the body's orientation in the
      * physical world.
-     * 
+     * </p>
+     * <p>
+     * The accelerometer is "calibrated" to return the
+     * acceleration of the object relative to the global inertial frame
+     * (the Earth). This means that an object at rest will record an
+     * acceleration with magnitude 0g, whereas an object in freefall
+     * will experience an acceleration of -1g. Note that this is
+     * different from the raw readings recorded by many real world
+     * accelerometers (1g at rest, 0g in freefall) that are not
+     * calibrated for use in an intertial navigation scenario.
+     * </p>
+     *
      * @return The acceleration along each body axis, (x, y, z), in m/s^2.
      */
     public Vector3f getLinearAcceleration();
