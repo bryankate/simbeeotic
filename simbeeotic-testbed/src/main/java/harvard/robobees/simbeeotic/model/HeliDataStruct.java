787bac74a3ddd1903d8086e7bf047a4a909f51b7
Karthik Dantu <kar@eecs.harvard.edu>
2013-05-12 20:24:05 -0400

    Added HeliDataStruct

---
+++ simbeeotic-testbed/src/main/java/harvard/robobees/simbeeotic/model/HeliDataStruct.java
@@ -0,0 +1,23 @@
+package harvard.robobees.simbeeotic.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kar
+ * Date: 5/12/13
+ * Time: 8:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HeliDataStruct {
+    long frameCount;
+    short[] process;
+    short[] gyros;
+    float[] debug;
+
+    HeliDataStruct() {
+        frameCount = 0;
+        process = new short[16];
+        gyros   = new short[3];
+        debug   = new float[4];
+    }
+}
+
